dc0d4dc43769d91d953f8d2908649bc7a243f6eb
LENOVO <raheesmohammed500@gmail.com>
2024-03-14 14:49:09 +0530

    colors

--- lib/features/constants/color_const.dart
+++ lib/features/constants/color_const.dart
@@ -1,5 +1,15 @@
 import 'dart:ui';
 
 class ColorConst{
-static const primaryColor=Color(242424);
+static const primaryColor=Color(0xff242424);
+static const secondaryColor=Color(0xffFFFFFF);
+static const grey=Color(0xff909090);
+static const green=Color(0xff27AE60);
+static const redGrey=Color(0xffB4916C);
+static const pink=Color(0xffE4CBAD);
+static const starYellow=Color(0xffF2C94C);
+static const cardYellow=Color(0xffF79E1B);
+static const cardred=Color(0xffEB001B);
+static const cardblack=Color(0xffFFFFFF0D);
+static const cardgrey=Color(0xff999999);
 }
--- lib/features/homescreen/screen/home_page.dart
+++ lib/features/homescreen/screen/home_page.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:furnigo/features/constants/color_const.dart';
 import 'package:furnigo/features/constants/icon_const.dart';
+
+import '../../../main.dart';
 
 class home extends StatefulWidget {
   const home({super.key});
@@ -13,8 +17,18 @@ class _homeState extends State<home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
-        leading: Image.asset(IconConst.searchIcon),
+        leading: Container(
+            width: w*0.05,
+            child: SvgPicture.asset(IconConst.searchIcon)),
+        title: Column(
+          children: [
+            Text("Make home",
+            style: TextStyle(color: ColorConst.grey),),
+            Text("BEAUTIFUL"),
+          ],
+        ),
       ),
     );
   }
