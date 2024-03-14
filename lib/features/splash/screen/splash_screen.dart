4c7b70ebff04d0140817934a35f40b5b1a328d08
shabi <shabimonu8932@gamil.com>
2024-03-14 12:37:00 +0530

    asset

--- lib/features/splash/screen/splash_screen.dart
+++ lib/features/splash/screen/splash_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:furnigo/features/constants/image_const.dart';
+
+import '../../../main.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -14,7 +17,11 @@ class _SplashScreenState extends State<SplashScreen> {
     return Scaffold(
       body: Column(
         children: [
-
+         Container(
+           height: h*1,
+           width: w*1,
+           child: Image.asset(ImageConst.sofa),
+         )
         ],
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:furnigo/features/splash/screen/splash_screen.dart';
+import 'package:google_fonts/google_fonts.dart';
 var h;
 var w;
 void main(){
@@ -22,7 +24,11 @@ class _MyAppState extends State<MyApp> {
         FocusManager.instance.primaryFocus?.unfocus();
       },
       child: MaterialApp(
+        theme: ThemeData(
+        textTheme: GoogleFonts.nunitoSansTextTheme()
+        ),
         debugShowCheckedModeBanner: false,
+        home: SplashScreen(),
       ),
     );
   }
