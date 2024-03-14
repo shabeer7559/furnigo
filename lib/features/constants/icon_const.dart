691dd09acc9abdfc890efb4f3d871cb36a0b0984
LENOVO <raheesmohammed500@gmail.com>
2024-03-14 12:27:45 +0530

    icons

--- lib/features/constants/icon_const.dart
+++ lib/features/constants/icon_const.dart
@@ -1,3 +1,5 @@
 class IconConst{
   static const homeIcon="assets/icon/clarity_home-solid.svg";
+  static const searchIcon="assets/icon/searchIcon.svg";
+  static const cartIcon="assets/icon/cartIcon.svg";
 }
---
+++ lib/features/homescreen/screen/home_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class home extends StatefulWidget {
+  const home({super.key});
+
+  @override
+  State<home> createState() => _homeState();
+}
+
+class _homeState extends State<home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        ,
+      ),
+    );
+  }
+}
