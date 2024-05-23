e277e55b2bffbc4cb0c1efe82a4d167d72a580f7
shabi <shabimonu8932@gamil.com>
2024-05-23 11:01:36 +0530

    backend edited in increament

--- lib/features/homescreen/screen/home_page.dart
+++ lib/features/homescreen/screen/home_page.dart
@@ -21,6 +21,7 @@ import '../../cart/screen/product_details.dart';
 import '../../splash/screen/splash_screen.dart';
 
 UserModel? currentUserModel;
+
 class home extends ConsumerStatefulWidget {
   const home({super.key});
 
@@ -29,9 +30,9 @@ class home extends ConsumerStatefulWidget {
 }
 
 class _homeState extends ConsumerState<home> {
-  int selectedIndex=0;
-  String docId="4zTSJ96StUheNnvi1UPH";
-  List bookMark=[];
+  int selectedIndex = 0;
+  String docId = "4zTSJ96StUheNnvi1UPH";
+  List bookMark = [];
 
   @override
   Widget build(BuildContext context) {
@@ -39,8 +40,10 @@ class _homeState extends ConsumerState<home> {
       backgroundColor: Colors.white,
       appBar: AppBar(
         leading: Padding(
-          padding:  EdgeInsets.all(w*0.04),
-          child: SvgPicture.asset(IconConst.searchIcon,),
+          padding: EdgeInsets.all(w * 0.04),
+          child: SvgPicture.asset(
+            IconConst.searchIcon,
+          ),
         ),
         title: Row(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -48,170 +51,193 @@ class _homeState extends ConsumerState<home> {
             Column(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                Text("Make home",
-                style: GoogleFonts.gelasio(color: ColorConst.grey),),
-                Text("BEAUTIFUL",
-                  style:GoogleFonts.gelasio(color:ColorConst.primaryColor,) ,),
+                Text(
+                  "Make home",
+                  style: GoogleFonts.gelasio(color: ColorConst.grey),
+                ),
+                Text(
+                  "BEAUTIFUL",
+                  style: GoogleFonts.gelasio(
+                    color: ColorConst.primaryColor,
+                  ),
+                ),
               ],
             ),
           ],
         ),
         actions: [
           InkWell(
-            onTap: () {
-              Navigator.push(context, CupertinoPageRoute(builder: (context) => MyCart(),));
-            },
+              onTap: () {
+                Navigator.push(
+                    context,
+                    CupertinoPageRoute(
+                      builder: (context) => MyCart(),
+                    ));
+              },
               child: SvgPicture.asset(IconConst.cartIcon)),
           SizedBox(
-            width: w*0.05,
+            width: w * 0.05,
           )
         ],
       ),
       body: Padding(
-        padding:  EdgeInsets.all(8.0),
+        padding: EdgeInsets.all(8.0),
         child: Column(
           children: [
             ref.watch(streamCategoryProvider).when(
-                data: (data) {
-
-                  return Container(
-                    width: w*1,
-                    height:h*0.15,
-                    child: ListView.separated(
-                      scrollDirection: Axis.horizontal,
-                      physics: BouncingScrollPhysics(),
-                      shrinkWrap: true,
-                      itemBuilder: (BuildContext context, int index) {
-                        return Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            InkWell(
-                              onTap: () {
-                                selectedIndex=index;
-                                docId=data[index].id;
-                                setState(() {
-
-                                });
-                              },
-                              child: Container(
-                                width: w*0.18,
-                                height: w*0.18,
-                                decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(w*0.04),
-                                    image: DecorationImage(image: NetworkImage(data[index].image.toString()),fit: BoxFit.fill,),
-                                    color:selectedIndex==index?ColorConst.primaryColor: ColorConst.secondaryColor,
+              data: (data) {
+                return Container(
+                  width: w * 1,
+                  height: h * 0.15,
+                  child: ListView.separated(
+                    scrollDirection: Axis.horizontal,
+                    physics: BouncingScrollPhysics(),
+                    shrinkWrap: true,
+                    itemBuilder: (BuildContext context, int index) {
+                      return Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          InkWell(
+                            onTap: () {
+                              selectedIndex = index;
+                              docId = data[index].id;
+                              setState(() {});
+                            },
+                            child: Container(
+                              width: w * 0.18,
+                              height: w * 0.18,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(w * 0.04),
+                                image: DecorationImage(
+                                  image: NetworkImage(
+                                      data[index].image.toString()),
+                                  fit: BoxFit.fill,
                                 ),
+                                color: selectedIndex == index
+                                    ? ColorConst.primaryColor
+                                    : ColorConst.secondaryColor,
                               ),
                             ),
-                            Text(data[index].category,style: TextStyle(
+                          ),
+                          Text(
+                            data[index].category,
+                            style: TextStyle(
                                 fontWeight: FontWeight.w500,
                                 color: ColorConst.grey,
-                                fontSize: w*0.04
-                            ),)
-                          ],
-                        );
+                                fontSize: w * 0.04),
+                          )
+                        ],
+                      );
+                    },
+                    separatorBuilder: (BuildContext context, int index) {
+                      return SizedBox(
+                        width: w * 0.03,
+                      );
+                    },
+                    itemCount: data.length,
+                  ),
+                );
+              },
+              error: (error, stackTrace) {
+                return Text(error.toString());
+              },
+              loading: () {
+                return Center(child: CircularProgressIndicator());
+              },
+            ),
+            ref.watch(streamProductsProvider(docId)).when(data: (data) {
+              return Expanded(
+                child: GridView.builder(
+                  physics: BouncingScrollPhysics(),
+                  itemCount: data.length,
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisSpacing: w * 0.03,
+                      mainAxisSpacing: w * 0.03,
+                      childAspectRatio: 0.7,
+                      crossAxisCount: 2),
+                  itemBuilder: (BuildContext context, int index) {
+                    return InkWell(
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            CupertinoPageRoute(
+                                builder: (context) => ProductDetails(
+                                    data[index].image.toString(),
+                                    data[index].name,
+                                    data[index].price,
+                                    data[index].review,
+                                    data[index].quantity,
+                                    docId,
+                                    data[index].id)));
                       },
-                      separatorBuilder: (BuildContext context, int index) {
-                        return SizedBox(width: w*0.03,);
-                      },
-                      itemCount: data.length,
-                    ),
-                  );
-                },
-                error: (error, stackTrace) {
-                  return Text(error.toString());
-                },
-                loading: () {
-                  return Center(child: CircularProgressIndicator());
-                },),
-            ref.watch(streamProductsProvider(docId)).when(
-                data: (data) {
-                  return   Expanded(
-                    child: GridView.builder(
-                      physics: BouncingScrollPhysics(),
-                      itemCount: data.length,
-                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                          crossAxisSpacing: w * 0.03,
-                          mainAxisSpacing: w* 0.03,
-                          childAspectRatio: 0.7,
-                          crossAxisCount: 2),
-                      itemBuilder: (BuildContext context, int index) {
-                        return InkWell(
-                          onTap: () {
-                            Navigator.push(context,   CupertinoPageRoute(builder: (context) => ProductDetails(
-                                data[index].image.toString(),
-                                data[index].name,
-                                data[index].price,
-                                data[index].review,
-                                data[index].quantity,
-                                 docId,
-                              data[index].id
-                            )));
-                          },
-                          child: Container(
-                            width: w*0.5,
-                            child: Column(
-                              crossAxisAlignment:CrossAxisAlignment.start ,
-                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                              children: [
-                                Stack(
-                                    children: [
-                                      Container(
-                                        height: h*0.25,
-                                        width: w*0.45,
-                                        decoration:BoxDecoration(
-                                            borderRadius: BorderRadius.circular(w*0.03),
-                                            image: DecorationImage(image: NetworkImage(data[index].image.toString()),fit: BoxFit.fill)
-
-                                        ) ,
-                                      ),
-                                      Positioned(
-                                        left: w*0.34,
-                                        bottom: w*0.05,
-                                        child: InkWell(
-                                          onTap:  () {
-                                            if (bookMark.contains(index)){
-                                              // bookMark.remove(index);
-                                            }else{
-                                              bookMark.add(index);
-                                              
-                                              print("---------------------------------------------------------");
-                                              ref.watch(homeScreenPro).favoriteAdd(userDocId, data[index].name, data[index].image, data[index].price);
-                                            }
-                                            setState(() {
-
-                                            });
-                                          },
-                                          child: SvgPicture.asset(bookMark.contains(index)?IconConst.blackcartIcon:IconConst.bookingIcon),
-                                        ),
-                                      )
-                                    ]
+                      child: Container(
+                        width: w * 0.5,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Stack(children: [
+                              Container(
+                                height: h * 0.25,
+                                width: w * 0.45,
+                                decoration: BoxDecoration(
+                                    borderRadius:
+                                        BorderRadius.circular(w * 0.03),
+                                    image: DecorationImage(
+                                        image: NetworkImage(
+                                            data[index].image.toString()),
+                                        fit: BoxFit.fill)),
+                              ),
+                              Positioned(
+                                left: w * 0.34,
+                                bottom: w * 0.05,
+                                child: InkWell(
+                                  onTap: () {
+                                    if (bookMark.contains(index)) {
+                                      // bookMark.remove(index);
+                                    } else {
+                                      bookMark.add(index);
+                                      print(
+                                          "---------------------------------------------------------");
+                                      ref.watch(homeScreenPro).favoriteAdd(
+                                          userDocId,
+                                          data[index].name,
+                                          data[index].image,
+                                          data[index].price);
+                                    }
+                                    setState(() {});
+                                  },
+                                  child: SvgPicture.asset(
+                                      bookMark.contains(index)
+                                          ? IconConst.blackcartIcon
+                                          : IconConst.bookingIcon),
                                 ),
-                                Text(data[index].name,style: TextStyle(
-                                    fontWeight: FontWeight.w500,
-                                    color: ColorConst.grey
-                                ),),
-                                Text(
-                            "\$ ${data[index].price.toString()}",
-                                  style: TextStyle(
-                                    fontWeight: FontWeight.w700,
-
-                                  ),)
-                              ],
+                              )
+                            ]),
+                            Text(
+                              data[index].name,
+                              style: TextStyle(
+                                  fontWeight: FontWeight.w500,
+                                  color: ColorConst.grey),
                             ),
-                          ),
-                        );
-                      },
-                    ),
-                  );
-                },
-                error: (error, stackTrace) {
-                  return Text(error.toString());
-                },
-                loading: () {
-                  return Center(child: CircularProgressIndicator());
-                }),
-
+                            Text(
+                              "\$ ${data[index].price.toString()}",
+                              style: TextStyle(
+                                fontWeight: FontWeight.w700,
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              );
+            }, error: (error, stackTrace) {
+              return Text(error.toString());
+            }, loading: () {
+              return Center(child: CircularProgressIndicator());
+            }),
           ],
         ),
       ),
